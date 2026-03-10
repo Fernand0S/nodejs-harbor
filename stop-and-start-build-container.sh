12b556b4d939354bc28d93af7a01d46888c185a0
Fernando Santana <32446123+Fernand0S@users.noreply.github.com>
2026-03-10 15:02:33 -0300

    stop-and-start-build-container.sh

--- stop-and-start-build-container.sh
+++ stop-and-start-build-container.sh
@@ -13,7 +13,7 @@ docker rm -f harbor-teams-webhook
  docker run -d \
 -p 5000:5000 \
 -e NODE_TLS_REJECT_UNAUTHORIZED=0 \
--e HARBOR_URL=https://harbor-qa.acelen.corp \
+-e HARBOR_URL=https://{URL} \
 -e HARBOR_USER='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' \
 -e HARBOR_PASSWORD=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX \
 -e TEAMS_WEBHOOK=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\                                                                                       955a67b00804d439be059782fe86f27/c0a43daa-7186-4eed-9e6d-162834c5ee18/V273cUiBjmi                                                                                        MF-qHOW47ItnvN3TG6KW4-QZynIEc1tmkI1 \
